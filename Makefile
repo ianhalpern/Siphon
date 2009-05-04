130d6221414a84805cb5f9d7af65d12f10c2625c
Ian Halpern <ian@ian-desktop>
2009-05-04 10:44:41 -0400

    modified build routine

--- Makefile
+++ Makefile
@@ -12,15 +12,20 @@ FNAME_QA="$(NAME)-$(VERNUM_QA)-qa"
 make:
 	mkdir -p $(BUILD_DIR)/$(FNAME)
 	bzr export $(BUILD_DIR)/$(FNAME)-export
+	cd $(BUILD_DIR)/$(FNAME)-export && build_rdf.py $(VERNUM)
 	cd $(BUILD_DIR)/$(FNAME)-export && cp -r $(REQ_FILES) $(REQ_DIRS) $(XTRA_FILES) ../$(FNAME)
 	cd $(BUILD_DIR)/$(FNAME) && zip -r ../$(FNAME).zip *
 	mv $(BUILD_DIR)/$(FNAME).zip $(BUILD_DIR)/$(FNAME).xpi
 
 qa:
 	mkdir -p $(BUILD_DIR)/$(FNAME_QA)
+	build_rdf.py $(VERNUM_QA)-qa
 	cp -r $(REQ_FILES) $(REQ_DIRS) $(XTRA_FILES) $(BUILD_DIR)/$(FNAME_QA)
 	cd $(BUILD_DIR)/$(FNAME_QA) && zip -r ../$(FNAME_QA).zip *
 	mv $(BUILD_DIR)/$(FNAME_QA).zip $(BUILD_DIR)/$(FNAME_QA).xpi
+
+rdf:
+	build_rdf.py $(VERNUM)
 
 clean:
 	rm -rf $(BUILD_DIR)
---
+++ build_rdf.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+import sys
+
+ID      = "siphon@siphon.ian-halpern.com"
+NAME    = "Siphon Extension Synchronizer"
+VERSION = sys.argv[ 1 ] or "UNKNOWN"
+DESC    = "Keep your favorite Extensions synced with all your computers."
+AUTHOR  = "Ian Halpern"
+WEBSITE = "http://siphon.ian-halpern.com"
+
+
+rdf = """
+<?xml version="1.0"?>
+<RDF xmlns="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+     xmlns:em="http://www.mozilla.org/2004/em-rdf#">
+
+	<Description about="urn:mozilla:install-manifest">
+
+		<em:id>%s</em:id>
+		<em:name>%s</em:name>
+		<em:version>%s</em:version>
+		<em:description>%s</em:description>
+		<em:creator>%s</em:creator>
+		<!-- optional items
+		<em:contributor>A person who helped you</em:contributor>
+		<em:contributor>Another one</em:contributor> -->
+		<em:homepageURL>%s</em:homepageURL>
+		<!--
+		<em:aboutURL>chrome://sampleext/content/about.xul</em:aboutURL>
+		<em:iconURL>chrome://sampleext/skin/mainicon.png</em:iconURL>
+		<em:updateURL>http://sampleextension.mozdev.org/update.rdf</em:updateURL-->
+		<em:optionsURL>chrome://siphon/content/options.xul</em:optionsURL>
+
+
+		<!-- Firefox -->
+		<em:targetApplication>
+		  <Description>
+			<em:id>{ec8030f7-c20a-464f-9b0e-13a3a9e97384}</em:id>
+			<em:minVersion>1.5</em:minVersion>
+			<em:maxVersion>3.0.*</em:maxVersion>
+		  </Description>
+		</em:targetApplication>
+
+	</Description>
+
+</RDF>
+""" % ( ID, NAME, VERSION, DESC, AUTHOR, WEBSITE )
+
+open( "install.rdf", "w" ).write( rdf )
